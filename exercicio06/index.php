3fce35ff18cdaed55e417386e5bde01526d14c54
Davi Mendes <davimendesduarte09@gmail.com>
2025-04-04 16:42:24 -0300

    Exercicios 02, 04, 06, 08 e 10 feito por Davi Mendes Duarte

---
+++ exercicio06/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 06</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Números pares</legend>
+        <form method="post" action="calcular.php">  
+            Digite um número:
+            <input type="text" name="num" size="25">
+            <br> <br>
+            <input type="submit" name="mostrarResultado" value="Mostrar palavra">
+        </form>
+    </fieldset>
+</body>
+
+</html>
